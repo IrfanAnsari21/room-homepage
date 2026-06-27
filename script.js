544263052ab91f964cc1badecd0f5b278738b391
IrfanAnsari21 <irfanansari.career@gmail.com>
2026-06-27 23:09:45 +0530

    Make slides, Implement styling and JS functionality

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ overlay.addEventListener("click", () => {
     navLinks.classList.remove("open");
     overlay.classList.remove("active");
 });
+
+
+// Slides
+const slides = document.querySelectorAll(".slide");
+var counter = 0;
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`
+});
+
+const prevSlide = () => {
+    if (counter > 0) {
+        counter--
+    } else if (counter === 0) {
+        counter = slides.length -1;
+    }
+    slideImage();
+}
+
+const nextSlide = () => {
+    if (counter === (slides.length - 1)) {
+        counter = 0;
+    } else {
+        counter++
+    }
+    slideImage();
+}
+
+const slideImage = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
